6cfafd655ecb76d5857185b7ff1ded9dccd2577e
Ken Rickard <agentrickard@gmail.com>
2021-05-25 09:49:20 -0400

    Initial commit of code examples.

---
+++ components/vuejs_v3_example/vuejs_v3_example.es6.js
@@ -0,0 +1,23 @@
+const MyVueApp = {
+  data() {
+    return {
+      greeting: 'Hello,',
+      name: 'Ken'
+    }
+  },
+  mounted: function() {
+    this.getConfiguration();
+  },
+  methods: {
+    getConfiguration() {
+      // Config is in the bounding class made by Component.
+      const config = document.querySelector('.vuejs_v3_example').dataset;
+      this.name = config.name;
+      if (config.greeting !== undefined) {
+        this.greeting = config.greeting;
+      }
+    },
+  },
+}
+
+Vue.createApp(MyVueApp).mount('.my-vue-app');
